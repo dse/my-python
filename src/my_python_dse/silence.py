7bf6fe69e4b1678d260069913af2aab53d01452e
Darren Embry <dse@webonastick.com>
2026-03-24 21:25:46 -0400

    rewrite; add disable() and is_silent() fns

--- src/my_python_dse/silence.py
+++ src/my_python_dse/silence.py
@@ -1,25 +1,36 @@
 import os
 
-stderr_fd = os.dup(2)
+stderr_fd = None                # also tells us if silent is on
 disabled = False
+
 if "DISABLE_SILENCE" in os.environ:
     disabled = True
 
 def silence(flag=True):
-    global disabled
-    global stderr_fd
     if disabled:
-        return
-    if flag is None:
+        pass
+    elif flag is None:
+        # Disable permanently.  Future attemps to turn silence off or
+        # back on do nothing.
+        silence(False)
         disabled = True
-        os.dup2(stderr_fd, 2)
-    elif flag:
-        os.close(2)
-    else:
-        os.dup2(stderr_fd, 2)
+    elif flag:                  # turn silence on
+        if stderr_fd is None:
+            stderr_fd = os.dup(2)
+            os.close(2)
+    else:                       # turn silence off
+        if stderr_fd is not None:
+            os.dup2(stderr_fd, 2)
+            stderr_fd = None
 
 def on():
     silence(True)
 
 def off():
     silence(False)
+
+def disable():
+    silence(None)
+
+def is_silent():
+    return stderr_fd is None
